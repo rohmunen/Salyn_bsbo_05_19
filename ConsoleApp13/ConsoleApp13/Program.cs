35517b2d85cafbf33557b45b9ba0752b637e984c
rohmunen <71789493+rohmunen@users.noreply.github.com>
2020-10-29 17:35:29 +0300

    ф

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        //()()(
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidParentheses("(())((()())())"));
+        }
+        public static bool ValidParentheses(string input)
+        {
+            if (input.Length == 0 || (!input.Contains("(")) || !input.Contains(")"))
+            {
+                return false;
+            }
+            int openingParantheses = 0;
+            int closingParantheses = 0;
+            foreach (var item in input)
+            {
+                if (item == '(')
+                {
+                    openingParantheses++;
+                }
+                if (item == ')')
+                {
+                    closingParantheses++;
+                }
+                if (openingParantheses < closingParantheses)
+                {
+                    return false;
+                }
+            }
+            if (openingParantheses != closingParantheses)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
